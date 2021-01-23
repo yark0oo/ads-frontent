8fe09b99ff89e8c34497ba8e48a8beade5ae21fe
yark0oo <72916841+yark0oo@users.noreply.github.com>
2021-01-23 12:03:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#modal").hide();
+
+
+
+    $("#add").click(function() {
+    $("#modal").show();
+    });
+
+    $("#close").click(function() {
+    $("#modal").hide();
+    });
+
+
+
+});
